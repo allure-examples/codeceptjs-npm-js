cbb8eb06e6ab8de78e807cd51506255c6bb8819a
qameta-ci <qameta-ci@qameta.io>
2024-09-27 17:10:39 +0000

    update 2024-09-27T17:10:39

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,13 +1,11 @@
 exports.config = {
-    tests: "./specs/*.spec.js",
-    helpers: {
-        Dummy: {
-            require: "./helpers/dummy.js"
-        }
+  name: "codeceptjs-npm-js",
+  tests: "./specs/*.spec.js",
+  output: "./output",
+  plugins: {
+    allure: {
+      outputDir: "./allure-results",
+      require: "allure-codeceptjs",
     },
-    plugins: {
-        allure: {
-            outputDir: "./allure-results"
-        }
-    }
+  },
 };
